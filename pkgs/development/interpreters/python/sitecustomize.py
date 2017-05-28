2444dbcc9504e23b4f3195377ac5da7fdc3400e6
Frederik Rietdijk <fridh@fridh.nl>
2017-05-28 18:05:49 +0200

    Python: use NIX_PYTHON_SCRIPT_NAME for setting sys.argv[0]

--- pkgs/development/interpreters/python/sitecustomize.py
+++ pkgs/development/interpreters/python/sitecustomize.py
@@ -12,3 +12,25 @@ import functools
 
 paths = os.environ.get('NIX_PYTHONPATH', '').split(':')
 functools.reduce(lambda k, p: site.addsitedir(p, k), paths, site._init_pathinfo())
+
+
+"""
+Set `sys.argv[0] of our script.
+"""
+import sys
+
+def argv_setter_hook(path):
+    if hasattr(sys, 'argv'):
+        # Remove this hook
+        sys.path_hooks.remove(argv_setter_hook)
+
+        import os
+        import functools
+        import site
+
+        sys.argv[0] = os.environ.get('NIX_PYTHON_SCRIPT_NAME', sys.argv[0])
+        functools.reduce(lambda k, p: site.addsitedir(p, k), paths, site._init_pathinfo())
+
+    raise ImportError # Let the real import machinery do its work
+
+sys.path_hooks[:0] = [argv_setter_hook]
