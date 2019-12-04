2e3ba28b7cea4d5838d897d4db9db3eeef5dba41
Frederik Rietdijk <fridh@fridh.nl>
2019-12-04 22:20:59 +0100

    windows.w32api: 3.17-2 -> 5.0.2

--- pkgs/os-specific/windows/w32api/default.nix
+++ pkgs/os-specific/windows/w32api/default.nix
@@ -1,11 +1,12 @@
 { stdenv, fetchurl, lib }:
 
 stdenv.mkDerivation rec {
-  name = "w32api-3.17-2";
+  pname = "w32api";
+  version = "5.0.2";
 
   src = fetchurl {
-    url = "mirror://sourceforge/mingw/MinGW/Base/w32api/w32api-3.17/${name}-mingw32-src.tar.lzma";
-    sha256 = "09rhnl6zikmdyb960im55jck0rdy5z9nlg3akx68ixn7khf3j8wb";
+    url = "mirror://sourceforge/mingw/MinGW/Base/w32api/${pname}-${version}/${pname}-${version}-mingw32-src.tar.lzma";
+    sha256 = "09rhnl6zikmdyb960im55jck0rdy5z9nlg3akx68ixn7khf3j8wa";
   };
 
   meta = {
