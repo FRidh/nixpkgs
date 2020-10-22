b71d6c6a40eb2ecda400610904727300769a9b11
Doron Behar <doron.behar@gmail.com>
2020-10-22 15:59:13 +0300

    kdeApplications: Use latest qt515 by default
    Fixes #100707 - Since KDE's maintainers don't allow non official kde
    applications under the `kdeApplications` path, many packages that depend
    on KDE libraries and applications are defined in all-packages.nix.
    Overriding all kdeApplications to use qt514 while all other
    `libsForQt5.callPackage` in all-packages.nix are using qt515, causes
    collisions.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13223,7 +13223,7 @@ in
     let
       mkFrameworks = import ../development/libraries/kde-frameworks;
       attrs = {
-        libsForQt5 = libsForQt514;
+        inherit libsForQt5;
         inherit lib fetchurl;
       };
     in
@@ -21583,7 +21583,7 @@ in
     let
       mkApplications = import ../applications/kde;
       attrs = {
-        libsForQt5 = libsForQt514;
+        inherit libsForQt5;
         inherit lib fetchurl;
         inherit okteta;
       };
@@ -25900,7 +25900,7 @@ in
     let
       mkPlasma5 = import ../desktops/plasma-5;
       attrs = {
-        libsForQt5 = libsForQt514;
+        inherit libsForQt5;
         inherit lib fetchurl;
         gconf = gnome2.GConf;
         inherit gsettings-desktop-schemas;
